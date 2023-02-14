d791e30c887c4faa8c0e562a61badd5834023ec6
u239983 <simon.pertschy@sbb.ch>
2023-02-14 08:42:52 +0100

    Added count

--- src/main/kotlin/com/example/ExampleResource.kt
+++ src/main/kotlin/com/example/ExampleResource.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import org.jboss.resteasy.reactive.RestPath
+import java.math.BigInteger
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -9,15 +10,28 @@ import javax.ws.rs.core.MediaType
 @Path("/")
 class ExampleResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello from RESTEasy Reactive"
+
 
 
     @GET
     @Path("/count/{number}")
-    @Produces(MediaType.TEXT_PLAIN)
-    fun count(@RestPath number: Int) = "Hello from RESTEasy Reactive $number"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun count(@RestPath number: Int): List<String> {
+        var t1: ULong = 0u
+        var t2: ULong = 1u
+        val serie = mutableListOf<String>()
+
+
+        for (i in 1..number) {
+            Thread.sleep(10)
+            serie.add(t1.toString())
+
+            val sum = t1 + t2
+            t1 = t2
+            t2 = sum
+        }
+        return serie
+    }
 
 
 }
