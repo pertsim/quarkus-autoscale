ebb8e791ef7fbd6fddd9ee29f1fac79767d458f4
u239983 <simon.pertschy@sbb.ch>
2023-02-14 09:30:42 +0100

    Config changed

--- src/test/kotlin/com/example/ExampleResourceTest.kt
+++ src/test/kotlin/com/example/ExampleResourceTest.kt
@@ -8,13 +8,6 @@ import org.junit.jupiter.api.Test
 @QuarkusTest
 class ExampleResourceTest {
 
-    @Test
-    fun testHelloEndpoint() {
-        given()
-                .`when`().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(`is`("Hello from RESTEasy Reactive"))
-    }
+
 
 }
